603845c73e68cf143e531cafc89f2efc92055295
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2024-04-15 22:31:34 +0200

    Add memberTree? function and test case

--- lab04/zad19.rkt
+++ lab04/zad19.rkt
@@ -1,15 +1,31 @@
 #lang racket
 
 
-; `(2 (3 () (4 () ())) (1 () ()))
 
 
 (define (memberTree? element tree)
     (cond
     [(null? tree) #f]
     [(eq? element (car tree)) #t]
+    [(list? (car tree)) (or (memberTree? element (car tree)) (memberTree? element (cdr tree)))]
     [else (memberTree? element (cdr tree))]
     )
 )
+(memberTree? 3 `(2 (3 () (4 () ())) (1 () ())))
 
-(memberTree? 1 `(2 (3 () (4 () ())) (1 () ())))
+(define (sumTree tree)
+    (cond
+    [(eq? tree `()) 0]
+    [(list? (car tree)) (+ (sumTree (car tree)) (sumTree (cdr tree)))]
+    [else (+ (sumTree (cdr tree)) (car tree))]
+    )
+)
+(sumTree `(2 (3 () (4 () ())) (1 () ())))
+
+(define (inorder tree)
+    (cond
+    [(null? tree) `()]
+    [else (append (inorder (cadr tree)) (cons (car tree) (inorder (caddr tree))))]
+    )
+)
+(inorder `(2 (3 () (4 () ())) (1 () ())))
